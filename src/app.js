cd9a3ed9e7e8971221c2bba7f028b23e6b6f18c8
Artem Pastukhov <artem.pastukhov@gmail.com>
2014-09-17 18:09:19 +0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,180 @@
+/**
+ *
+ *
+ * http://pebble.github.io/pebblejs/#settings
+ *
+ */
+
+var Settings = require('settings');
+var ajax = require('ajax');
+
+
+var Base64 = {
+    // private property
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    // public method for encoding
+    encode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+        input = Base64._utf8_encode(input);
+        while (i < input.length) {
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            output = output +
+            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+        }
+        return output;
+    },
+    // public method for decoding
+    decode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+        while (i < input.length) {
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+            output = output + String.fromCharCode(chr1);
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+        }
+        output = Base64._utf8_decode(output);
+        return output;
+    },
+    // private method for UTF-8 encoding
+    _utf8_encode : function (string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        return utftext;
+    },
+    // private method for UTF-8 decoding
+    _utf8_decode : function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = 0;
+//        var c1 = 0;
+        var c2 = 0;
+        var c3 = 0;
+        while ( i < utftext.length ) {
+            c = utftext.charCodeAt(i);
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i+1);
+                c3 = utftext.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        }
+        return string;
+    }
+};
+
+var domoticz = {
+	_data : {},
+    devices : function () {
+
+                  ajax(
+                  {
+                  url: Settings.option('url') + '/json.htm?type=devices&filter=light&used=true&order=Name',
+                  type: 'json',
+                  async: false,
+                  headers:{
+                      Authorization: "Basic " + Base64.encode(Settings.option('login') + ":" + Settings.option('password')),
+                      },
+                  },
+                      function(data) {
+                          domoticz._data = data;
+//                          console.log('Domoticz status inside object: ' + domoticz._data.status);
+                      },
+                      function(error) {
+                          console.log('The ajax request failed: ' + error);
+                      }
+                  );
+                  return domoticz._data;
+                  }
+    };
+
+
+
+
+var UI = require('ui');
+
+Settings.config(
+  { url: 'https://cloud.nayg.ru/domoticz.html' },
+	function(e) {console.log('opening configurable');},
+	function(e) {console.log('closed configurable');if (e.failed) {console.log(e.response);}}
+  );
+
+    var UI = require('ui');
+    var main = new UI.Menu();
+    var devices = domoticz.devices();
+
+for(var i=0; i<devices.result.length; i++) {
+        main.item(0, i, { title: devices.result[i].Name});
+        console.log(devices.result[i].Name + "   ТЕСТ");
+}
+
+
+
+
+    main.show();
+
+
+
+
+
+
+var devices = domoticz.devices();
+	console.log("devices: " + JSON.stringify(devices));
+
+
+console.log('Domoticz status on app: ' + devices.status);
+
+
+
+
